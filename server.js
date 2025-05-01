84a59a0c0d140b6bd37bc80e71684e8a4c81357e
unknown <juymc1974@gmail.com>
2025-05-01 22:37:54 +0700

    ymcshop-api-Edit-SEO

--- server.js
+++ server.js
@@ -4,23 +4,13 @@ const app = express()
 const morgan = require('morgan')
 const { readdirSync } = require('fs')
 const cors = require('cors')
-// const prerender = require('prerender-node') // ✅ เพิ่มตรงนี้
 // const authRouter = require('./routes/auth')
 // const categoryRouter = require('./routes/category')
-
-// prerender.set('prerenderToken', 'MJVQROCR6ltrN10nVTn3')
-// app.use(prerender) // ✅ ใช้ middleware ก่อน router
 
 // middleware
 app.use(morgan('dev'))
 app.use(express.json({ limit: '20mb' }))
 app.use(cors())
-// app.use(cors({
-//     origin: ['https://ymcshop.vercel.app'], // ✅ ใส่ origin ของ frontend ให้ชัดเจน
-//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//     credentials: true
-//   }))
-
 // app.use('/api',authRouter)
 // app.use('/api',categoryRouter)
 readdirSync('./routes')
